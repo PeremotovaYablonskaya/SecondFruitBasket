c884651f059c84664e623553bd74f36698c37dc8
PeremotovaYablonskaya <ann.peremotova@gmail.com>
2015-09-26 08:33:04 +0300

    Created by Coding Ground Client

---
+++ New Project/PYFruit.h
@@ -0,0 +1,14 @@
+//
+//  PYFruit.h
+//  SecondFruitBasket
+//
+//  Created by fpmi on 19.09.15.
+//  Copyright (c) 2015 fpmi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PYFruit : NSObject
+@property int weight;
+-(id)initWithWeight: (int) aWeight;
+@end
